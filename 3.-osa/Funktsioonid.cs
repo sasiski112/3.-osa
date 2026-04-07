52cf6061415833bf51de1deed2349075aab5a711
sasiski112 <mshevelovv@gmail.com>
2026-04-07 14:16:11 +0300

    Update Funktsioonid.cs

--- 3.-osa/Funktsioonid.cs
+++ 3.-osa/Funktsioonid.cs
@@ -1,44 +1,59 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace _3._osa
 {
-    internal class Funktsioonid
+    internal class Funktsioon
     {
-        // 1
+        // 1. Ruudud
         public static void Ruudud()
         {
-            try
-            {
-                Random r = new Random();
-                int a = r.Next(1, 10);
-                int b = r.Next(1, 10);
+            Random r = new Random();
+            int a = r.Next(1, 10);
+            int b = r.Next(1, 10);
 
-                int min = Math.Min(a, b);
-                int max = Math.Max(a, b);
+            int min = Math.Min(a, b);
+            int max = Math.Max(a, b);
 
-                for (int i = min; i <= max; i++)
-                    Console.WriteLine($"{i} → {i * i}");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
+            for (int i = min; i <= max; i++)
+                Console.WriteLine($"{i} - {i * i}");
         }
 
-        // 2
+        // 2. KäivitaAnalüüs
         public static void KäivitaAnalüüs()
         {
-            try 
-            { 
-                Console.WriteLine("Sisesta arvud:");
-                double[] arvud = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Console.WriteLine("Sisesta arvud (ruumiga eraldatud):");
 
-                var t = Analüüsi(arvud);
-                Console.WriteLine($"Summa:{t.Summa} Keskmine:{t.Keskmine} Korrutis:{t.Korrutis}");
-        } 
-            catch (Exception e)
-            { Console.WriteLine(e) ; }
-}
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Tühi sisend!");
+                return;
+            }
+
+            string[] osad = input.Split();
+            List<double> arvud = new List<double>();
+
+            foreach (var s in osad)
+            {
+                if (double.TryParse(s, out double x))
+                    arvud.Add(x);
+                else
+                    Console.WriteLine($"Vale arv: {s}");
+            }
+
+            if (arvud.Count == 0)
+            {
+                Console.WriteLine("Pole ühtegi korrektset arvu!");
+                return;
+            }
+
+            var t = Analüüsi(arvud.ToArray());
+            Console.WriteLine($"Summa: {t.Summa}");
+            Console.WriteLine($"Keskmine: {t.Keskmine}");
+            Console.WriteLine($"Korrutis: {t.Korrutis}");
+        }
 
         static (double Summa, double Keskmine, double Korrutis) Analüüsi(double[] a)
         {
@@ -49,203 +64,188 @@ namespace _3._osa
             return (s, k, korr);
         }
 
-        // 3
+        // 3. Inimesed
         class Inimene
         {
             public string Nimi;
             public int Vanus;
         }
+
         public static void Inimesed()
         {
-            Console.WriteLine("Kirjuta Nimi ja Vanus 5 inimeseid");
-            try { 
+            Console.WriteLine("Sisesta 5 inimest:");
             List<Inimene> list = new List<Inimene>();
 
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Nimi: ");
                 string nimi = Console.ReadLine();
-                Console.Write("Vanus: ");
-                int vanus = int.Parse(Console.ReadLine());
+
+                int vanus;
+                while (true)
+                {
+                    Console.Write("Vanus: ");
+                    if (int.TryParse(Console.ReadLine(), out vanus) && vanus > 0)
+                        break;
+                    Console.WriteLine("Vale vanus! Proovi uuesti.");
+                }
 
                 list.Add(new Inimene { Nimi = nimi, Vanus = vanus });
-
             }
 
-            var sum = list.Sum(x => x.Vanus);
-            var avg = list.Average(x => x.Vanus);
-            var max = list.OrderByDescending(x => x.Vanus).First();
-            var min = list.OrderBy(x => x.Vanus).First();
-
-            Console.WriteLine($"Sum:{sum} Avg:{avg}");
-            Console.WriteLine($"Vanim:{max.Nimi} Noorim:{min.Nimi}");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
+            Console.WriteLine($"Keskmine vanus: {list.Average(x => x.Vanus)}");
+            var vanim = list.OrderByDescending(x => x.Vanus).First();
+            var noorim = list.OrderBy(x => x.Vanus).First();
+            Console.WriteLine($"Vanim: {vanim.Nimi}, Noorim: {noorim.Nimi}");
         }
 
-
-
-        // 4
+        // 4. Märksõna
         public static void Märksõna()
         {
-            try { 
             List<string> list = new List<string>();
             string s;
 
             do
             {
-                    Console.WriteLine("Osta elevant");
+                Console.WriteLine("Osta elevant");
                 Console.Write("Sisesta: ");
                 s = Console.ReadLine();
                 list.Add(s);
             } while (s != "stop");
 
+            Console.WriteLine("Sisestatud sõnad:");
             foreach (var x in list) Console.WriteLine(x);
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
         }
 
-        // 5
+        // 5. ArvaArv
         public static void ArvaArv()
         {
-            Console.WriteLine("Arva ära number vahemikus 1 kuni 100 viie katsega!");
-            try { 
+            Console.WriteLine("Arva number 1–100 (5 katset)");
             Random r = new Random();
-            int arv = r.Next(1, 100);
+            int arv = r.Next(1, 101);
 
             for (int i = 0; i < 5; i++)
             {
-                int g = int.Parse(Console.ReadLine());
+                Console.Write("Sinu arv: ");
+                if (!int.TryParse(Console.ReadLine(), out int g))
+                {
+                    Console.WriteLine("Vale sisend! Sisesta arv.");
+                    i--; // ei loe katset
+                    continue;
+                }
 
-                if (g == arv) { Console.WriteLine("Õige"); return; }
-                else if (g > arv) Console.WriteLine("Liiga suur");
-                else Console.WriteLine("Liiga väike");
+                if (g == arv)
+                {
+                    Console.WriteLine("Õige!");
+                    return;
+                }
+                else if (g > arv)
+                    Console.WriteLine("Liiga suur");
+                else
+                    Console.WriteLine("Liiga väike");
             }
+
             Console.WriteLine($"Õige oli {arv}");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
         }
 
-        // 6
+        // 6. SuurimArv
         public static void SuurimArv()
-        { try {
-                Console.WriteLine("Sisesta 4 numbrit:");
-                int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-
-                Array.Sort(a);
-                Array.Reverse(a);
-
-                Console.WriteLine(string.Join("", a));
+        {
+            Console.WriteLine("Sisesta 4 numbrit (ruumiga eraldatud):");
+            string[] sisend = Console.ReadLine().Split();
+            if (sisend.Length != 4)
+            {
+                Console.WriteLine("Peab olema 4 arvu!");
+                return;
             }
-            catch (Exception e)
-            { Console.WriteLine(e);
-            } 
+
+            int[] a = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                if (!int.TryParse(sisend[i], out a[i]))
+                {
+                    Console.WriteLine("Vale arv!");
+                    return;
+                }
+            }
+
+            Array.Sort(a);
+            Array.Reverse(a);
+            Console.WriteLine("Järjestatud suurimast väiksemani: " + string.Join(" ", a));
         }
 
-        // 7
+        // 7. Korrutustabel
         public static void Korrutustabel()
         {
-            try
-            {
-                for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 for (int j = 1; j <= 10; j++)
                     Console.Write((i * j).ToString().PadLeft(4));
                 Console.WriteLine();
             }
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
         }
 
-        // 8
+        // 8. Tudengid
         public static void Tudengid()
         {
-            try
-            {
-                List<string> n = new List<string>
-        { "Andres","Mari","Jaan","Kati","Peeter","Liis","Anne","Karl","Ain","Mati" };
+            List<string> n = new List<string>
+            { "Andres","Mari","Jaan","Kati","Peeter","Liis","Anne","Karl","Ain","Mati" };
 
-            n[2] = "Kati";
+            n[2] = "Kati"; // muutmine
             n[5] = "Mati";
 
-            int i = 0;
-            while (i < n.Count)
-            {
-                if (n[i].StartsWith("A"))
-                    Console.WriteLine(n[i]);
-                i++;
-            }
-                Console.WriteLine();
+            Console.WriteLine("Nimed, mis algavad A-tähega:");
+            foreach (var x in n.Where(x => x.StartsWith("A")))
+                Console.WriteLine(x);
+
+            Console.WriteLine("Kõik nimed indeksitega:");
             for (int j = 0; j < n.Count; j++)
-                Console.WriteLine($"{j}:{n[j]}");
-                Console.WriteLine();
+                Console.WriteLine($"{j}: {n[j]}");
+
+            Console.WriteLine("Kõik nimed väikeste tähtedega:");
             foreach (var x in n)
                 Console.WriteLine(x.ToLower());
-                Console.WriteLine();
+
+            Console.WriteLine("Väljasta kuni 'Mati':");
             int k = 0;
             do
             {
                 Console.WriteLine(n[k]);
-            } while (n[k++] != "Mati");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
+            } while (n[k++] != "Mati" && k < n.Count);
         }
 
-        // 9
+        // 9. ArvRud
         public static void ArvRud()
         {
-            try { 
             int[] a = { 2, 4, 6, 8, 10, 12 };
 
-            for (int i = 0; i < a.Length; i++)
-                Console.WriteLine(a[i] * a[i]);
+            Console.WriteLine("Iga ruut:");
+            foreach (var x in a)
+                Console.WriteLine(x * x);
 
+            Console.WriteLine("Iga arv kahekordne:");
             foreach (var x in a)
                 Console.WriteLine(x * 2);
 
-            int c = 0, i2 = 0;
-            while (i2 < a.Length)
-            {
-                if (a[i2] % 3 == 0) c++;
-                i2++;
-            }
-            Console.WriteLine(c);
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
+            int count3 = a.Count(x => x % 3 == 0);
+            Console.WriteLine($"Arvud, mis jaguvad 3-ga: {count3}");
         }
 
-        // 10
+        // 10. PositivneNegativne
         public static void PositivneNegativne()
         {
-            try { 
             int[] a = { 5, -3, 0, 8, -1, 4, -7, 2, 0, -5, 6, 9 };
 
-            int p = 0, n = 0, z = 0;
-
-            foreach (var x in a)
-            {
-                if (x > 0) p++;
-                else if (x < 0) n++;
-                else z++;
-            }
+            int p = a.Count(x => x > 0);
+            int n = a.Count(x => x < 0);
+            int z = a.Count(x => x == 0);
 
             Console.WriteLine($"Pos:{p} Neg:{n} Zero:{z}");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
         }
 
-        // 11
+        // 11. Keskmine
         public static void Keskmine()
         {
-            try { 
             Random r = new Random();
             int[] a = new int[15];
 
@@ -253,75 +253,45 @@ namespace _3._osa
                 a[i] = r.Next(1, 101);
 
             double avg = a.Average();
-            Console.WriteLine(avg);
+            Console.WriteLine($"Keskmine: {avg}");
 
-            foreach (var x in a)
-                if (x > avg) Console.WriteLine(x);
+            Console.WriteLine("Suuremad kui keskmine:");
+            foreach (var x in a.Where(x => x > avg))
+                Console.WriteLine(x);
 
             int i2 = 0;
             do
             {
                 Console.WriteLine(a[i2]);
             } while (a[i2++] >= 10 && i2 < a.Length);
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
         }
 
-        // 12
+        // 12. KõigeSuurem
         public static void KõigeSuurem()
         {
-            try
-            {
-                int[] a = { 12, 56, 78, 2, 90, 43, 88, 67 };
+            int[] a = { 12, 56, 78, 2, 90, 43, 88, 67 };
 
-            int max = a[0], idx = 0;
+            int max = a.Max();
+            int idx = Array.IndexOf(a, max);
 
-            for (int i = 1; i < a.Length; i++)
-            {
-                if (a[i] > max)
-                {
-                    max = a[i];
-                    idx = i;
-                }
-            }
-
-            Console.WriteLine($"Max:{max} Index:{idx}");
-            }
-            catch (Exception e)
-            { Console.WriteLine(e); }
+            Console.WriteLine($"Max: {max} Index: {idx}");
         }
 
-        // 13
+        // 13. PaariLoendused
         public static void PaariLoendused()
         {
-            try { 
             Random r = new Random();
             List<int> list = new List<int>();
 
             for (int i = 0; i < 20; i++)
                 list.Add(r.Next(1, 101));
 
-            int sumE = 0;
-            for (int i = 0; i < list.Count; i++)
-                if (list[i] % 2 == 0) sumE += list[i];
+            int sumE = list.Where(x => x % 2 == 0).Sum();
+            var oddList = list.Where(x => x % 2 != 0).ToList();
+            double avgO = oddList.Count > 0 ? oddList.Average() : 0;
+            int c50 = list.Count(x => x > 50);
 
-            int sumO = 0, countO = 0;
-            foreach (var x in list)
-                if (x % 2 != 0) { sumO += x; countO++; }
-
-            double avgO = (double)sumO / countO;
-
-            int c50 = 0, i2 = 0;
-            while (i2 < list.Count)
-            {
-                if (list[i2] > 50) c50++;
-                i2++;
-            }
-
-            Console.WriteLine($"Even:{sumE} OddAvg:{avgO} >50:{c50}");
-                 } catch (Exception e)
-            { Console.WriteLine(e) ; }
+            Console.WriteLine($"Even sum: {sumE}, Odd avg: {avgO}, >50: {c50}");
         }
     }
 }
